f140c696d7c022d39e29331163f665dbb725363f
WebDeveloperM <webdevelopermm1514@gmail.com>
2021-10-14 15:46:26 +0500

    DSFDSFDS
    SFFSDFDS

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,6 +21,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
